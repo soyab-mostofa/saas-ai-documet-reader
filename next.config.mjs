494238765e20e742a62143133ac45b6a497462f6
soyab-mostofa <soyabmostofa@gmail.com>
2024-06-22 20:37:34 +0600

    fix: Missing Suspense boundary with useSearchParams

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
   webpack: (config) => {
     config.resolve.alias.canvas = false;
 
