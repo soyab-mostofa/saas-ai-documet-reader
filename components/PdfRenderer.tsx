482577dad36ac571b8f6006129604920b0114fa2
soyab-mostofa <soyabmostofa@gmail.com>
2024-06-22 20:20:18 +0600

    fixed pdf viewer

--- components/PdfRenderer.tsx
+++ components/PdfRenderer.tsx
@@ -1,12 +1,16 @@
 "use client";
+import { Loader2 } from "lucide-react";
 import React from "react";
 import { Document, Page } from "react-pdf";
 import { pdfjs } from "react-pdf";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/legacy/build/pdf.worker.min.mjs",
   import.meta.url,
 ).toString();
+
 interface PdfRendererProps {
   url: string;
 }
@@ -19,7 +23,15 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
 
       <div className="max-h-screen w-full flex-1">
         <div>
-          <Document className="max-h-full" file={url}>
+          <Document
+            loading={
+              <div className="flex h-full w-full items-center justify-center">
+                <Loader2 className="animate my-24 h-6 w-6" />
+              </div>
+            }
+            className="max-h-full"
+            file={url}
+          >
             <Page pageNumber={1} />
           </Document>
         </div>
